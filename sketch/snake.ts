df7baeeabe48d4a8d7990e3343e1581a3d80b581
David Jensen <Tsourdox@icloud.com>
2019-08-26 00:47:43 +0200

    remove snake when dead + no semicolon

--- sketch/snake.ts
+++ sketch/snake.ts
@@ -1,81 +1,83 @@
 class Snake extends GameObject {
-    public readonly id: number;
-    public readonly name: string;
-    private readonly color: p5.Color;
-    private readonly controls: Controls;
-    private direction!: number;
-    public isAlive!: boolean;
-    public thickness!: number;
-    public body!: Point[];
+    public readonly id: number
+    public readonly name: string
+    private readonly color: p5.Color
+    private readonly controls: Controls
+    private direction!: number
+    public isAlive!: boolean
+    public thickness!: number
+    public body!: Point[]
 
     constructor(name: string, _color: string, controls: Controls) {
-        super();
-        this.id = random(0, 999999);
-        this.name = name;
-        this.color = color(_color);
+        super()
+        this.id = random(0, 999999)
+        this.name = name
+        this.color = color(_color)
         this.controls = controls
-        this.birth();
+        this.birth()
     }
 
     private birth() {
-        const paddingX = width * 0.1;
-        const paddingY = height * 0.1;
+        const paddingX = width * 0.1
+        const paddingY = height * 0.1
         this.body = [{
             x: paddingX + random() * (width - paddingX * 2),
             y: paddingY + random() * (height - paddingY * 2)
         }]
-        this.direction = random(0, 360);
-        this.thickness = 5;
-        this.isAlive = true;
+        this.direction = random(0, 360)
+        this.thickness = 5
+        this.isAlive = true
     }
 
     public get head() {
-        return this.body[this.body.length - 1];
+        return this.body[this.body.length - 1]
     }
 
     private get tail() {
-        return this.body[0];
+        return this.body[0]
     }
 
     public update() {
         if (this.isAlive) {
-            this.updateDirection();
-            this.growBody();
+            this.updateDirection()
+            this.growBody()
+        } else {
+            this.shrinkBody()
         }
     }
 
     public draw() {
         if (game.isPaused) {
-            this.drawHead();
+            this.drawHead()
         }
-        this.drawBody();
+        this.drawBody()
     }
 
     private drawHead() {
-        const { x, y } = this.head;
-        noStroke();
-        fill(this.color);
-        circle(x, y, this.thickness);
-        noFill();
-        stroke(this.color);
-        strokeWeight(this.thickness * 0.5);
-        circle(x, y, this.thickness * 4);
+        const { x, y } = this.head
+        noStroke()
+        fill(this.color)
+        circle(x, y, this.thickness)
+        noFill()
+        stroke(this.color)
+        strokeWeight(this.thickness * 0.5)
+        circle(x, y, this.thickness * 4)
     }
 
     private drawBody() {
-        stroke(this.color);
-        strokeWeight(this.thickness);
-        curveTightness(0.5);
-        noFill();
+        stroke(this.color)
+        strokeWeight(this.thickness)
+        curveTightness(0.5)
+        noFill()
 
-        beginShape();
-        curveVertex(this.tail.x, this.tail.y);
+        beginShape()
+        curveVertex(this.tail.x, this.tail.y)
         for (let i = 0; i < this.body.length; i++) {
-            const { x, y } = this.body[i];
-            curveVertex(x, y);
+            const { x, y } = this.body[i]
+            curveVertex(x, y)
         }
-        curveVertex(this.head.x, this.head.y);
-        endShape();
+        curveVertex(this.head.x, this.head.y)
+        endShape()
     }
 
     private updateDirection() {
@@ -87,10 +89,16 @@ class Snake extends GameObject {
     }
 
     private growBody() {
-        const { x, y } = this.body[this.body.length - 1];
+        const { x, y } = this.body[this.body.length - 1]
         this.body.push({
             x: x + cos(this.direction) * 1,
             y: y + sin(this.direction) * 1
         })
+    }
+
+    private shrinkBody() {
+        if (this.body.length > 1) {
+            this.body.shift()
+        }
     }
 }
