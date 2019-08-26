6b5c9bdf06be338520ff7ee6b5d484fb530324b8
David Jensen <Tsourdox@icloud.com>
2019-08-26 02:26:46 +0200

    clean up menu code

--- sketch/menu.ts
+++ sketch/menu.ts
@@ -1,12 +1,18 @@
+type SetupStep = 'start' | 'number-of-players' | 'done'
+
 class Menu {
     private bgColor: p5.Color
     private textColor: p5.Color
-    public isSetup: boolean
+    public setupStep: SetupStep
+
+    public get isSetup() {
+        return this.setupStep != 'done'
+    }
 
     constructor() {
         this.bgColor = color(0, 160)
         this.textColor = color(180)
-        this.isSetup = true
+        this.setupStep = 'start'
     }
 
     public draw() {
@@ -19,36 +25,57 @@ class Menu {
             const y = height * 0.5
 
             // Pepare drawing
-            textAlign(CENTER, CENTER)
             noStroke()
 
             // Draw circle
             fill(this.bgColor)
             circle(width * 0.5, height * 0.5, diameter)
 
-            // Draw main menu text
+            // Pepare text
+            textAlign(CENTER, CENTER)
             fill(this.textColor)
-            textSize(diameter * 0.08)
-            text('MAIN MENU', x, y - diameter * 0.25)
 
-            // Draw content
-            if (this.isSetup) {
-                textSize(diameter * 0.05)
-                text('How many players are you?', x, y + diameter * 0.07)
-
+            if (this.setupStep == 'start') {
                 textSize(diameter * 0.07)
-                text('1-6', x, y + diameter * 0.2)
-            } else {
-                textSize(diameter * 0.04)
-                textSize(diameter * 0.04)
-                text('press enter to restart round', x, y - diameter * 0.02)
-                textSize(diameter * 0.042)
                 textStyle(BOLD)
-                text('press space to play/pause the game', x, y + diameter * 0.1)
+                text('press space to begin', x, y)
                 textStyle(NORMAL)
-                textSize(diameter * 0.04)
-                text('press esc to select players', x, y + diameter * 0.22)
+
+            } else {
+                this.drawMainMenuHeader(x, y, diameter)
+
+                // Draw content
+                if (this.setupStep == 'number-of-players') {
+                    this.askForNumberOfPlayers(x, y, diameter)
+                } else {
+                    this.printActionList(x, y, diameter)
+                }
             }
         }
     }
+
+    private drawMainMenuHeader(x: number, y: number, diameter: number) {
+        textSize(diameter * 0.08)
+        text('MAIN MENU', x, y - diameter * 0.25)
+    }
+
+    private askForNumberOfPlayers(x: number, y: number, diameter: number) {
+        textSize(diameter * 0.05)
+        text('How many players are you?', x, y + diameter * 0.07)
+
+        textSize(diameter * 0.07)
+        text('1-6', x, y + diameter * 0.2)
+    }
+
+    private printActionList(x: number, y: number, diameter: number) {
+        textSize(diameter * 0.04)
+        text('press enter to restart round', x, y - diameter * 0.02)
+        textSize(diameter * 0.042)
+        textStyle(BOLD)
+        text('press space to play/pause the game', x, y + diameter * 0.1)
+        textStyle(NORMAL)
+        textSize(diameter * 0.04)
+        text('press esc to select players', x, y + diameter * 0.22)
+    }
+
 }
