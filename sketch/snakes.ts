df63180a94f57d73dc006faf2da0bc859fa68af3
David Jensen <Tsourdox@icloud.com>
2019-08-31 13:09:38 +0200

    restruct snakes file for better readability

--- sketch/snakes.ts
+++ sketch/snakes.ts
@@ -1,54 +1,24 @@
 class Snakes {
     public static get all() {
         return [
-            new Snake('Bliz', 'blue',
-                {
-                    left: LEFT_ARROW,
-                    special: UP_ARROW,
-                    right: RIGHT_ARROW
-                },
-                new FreezeAbility(14, 2.4)
-            ),
-            new Snake('Mess', 'red',
-                {
-                    left: KEY_Z,
-                    special: KEY_X,
-                    right: KEY_C
-                },
-                new BurnAbility(12, 1.7)
-            ),
-            new Snake('Glow', 'yellow',
-                {
-                    left: KEY_Y,
-                    special: KEY_U,
-                    right: KEY_I
-                },
-                new RebirthAbility(6)
-            ),
-            new Snake('Dash', 'green',
-                {
-                    left: KEY_Q,
-                    special: KEY_W,
-                    right: KEY_E
-                },
-                new TeleportAbility(4))
-            ,
-            new Snake('Ouc', 'purple',
-                {
-                    left: KEY_B,
-                    special: KEY_N,
-                    right: KEY_M
-                },
-                new FreezeAbility(6, 1.3)
-            ),
-            new Snake('Triz', 'orange',
-                {
-                    left: KEY_P,
-                    special: KEY_Å,
-                    right: KEY_POW_CHAR,
-                },
-                new BurnAbility(16, 2.5)
-            )
+            new Snake('Bliz', 'blue', this.controls[0], new FreezeAbility(14, 2.4)),
+            new Snake('Mess', 'red', this.controls[1], new BurnAbility(12, 1.7)),
+            new Snake('Glow', 'yellow', this.controls[2], new RebirthAbility(4)),
+            new Snake('Dash', 'green', this.controls[3], new TeleportAbility(5)),
+
+            new Snake('Ouc', 'purple', this.controls[4], new FreezeAbility(6, 1.3)),
+            new Snake('Triz', 'orange', this.controls[5], new RebirthAbility(4))
+        ]
+    }
+
+    private static get controls() {
+        return [
+            { left: LEFT_ARROW, special: UP_ARROW, right: RIGHT_ARROW },
+            { left: KEY_Z, special: KEY_X, right: KEY_C },
+            { left: KEY_Y, special: KEY_U, right: KEY_I },
+            { left: KEY_Q, special: KEY_W, right: KEY_E },
+            { left: KEY_B, special: KEY_N, right: KEY_M },
+            { left: KEY_P, special: KEY_Å, right: KEY_POW_CHAR, }
         ]
     }
 }
